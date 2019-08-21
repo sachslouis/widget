5279c63400d1b1e0433241607005e0d19de3e0e3
Louis <sachslouis@gmail.com>
2019-08-21 20:38:57 +0300

    css tweaks for responsiveness

--- widget.js
+++ widget.js
@@ -4,7 +4,7 @@ function click(){
 
 var pictureArray = ["img/banana.jpeg", "img/walrus.jpeg", "img/moustache.jpg", "img/belly.jpeg", "img/trash.jpeg", "img/cat.jpeg"];
 var clickbaitArray = ["Click here to find out how you are going to die", "Do you know what Beatles songs REALLY mean?", "What animal do you look like?", "10 of the hottest summer fashion trends you never saw coming", "This deadly mold is growing in your garbage can right now!", "Friend or enemy. Take this quiz to see if you can spot the difference"]
-var advertiserArray=["Brought to you by the letter E", "BuzzDrugs", "WhoRU | Sponsored Links", "ClickBatesMotel", "MindHatch", "Kinks College"]
+var brandingArray=["Brought to you by the letter E", "BuzzDrugs", "WhoRU | Sponsored Links", "ClickBatesMotel", "MindHatch", "Kinks College"]
 var linkArray=["https://www.gotoquiz.com/how_am_i_going_to_die", "https://allthatsinteresting.com/beatles-songs", "https://www.quizony.com/what-animal-do-i-look-like/1.html", "https://www.thetrendspotter.net/fashion-trends-spring-summer-2019/", "https://gleampressurewashing.com/5-worst-things-growing-trash-can/", "https://www.proprofs.com/quiz-school/story.php?title=frends-enemies"];
 var widget = document.createElement("div");
 widget.id="widget";
@@ -42,15 +42,16 @@ for(i=0; i<pictureArray.length; i++){
     var itemTitle=document.createElement("div");
     itemTitle.className="itemTitle";
     itemTitle.innerHTML=clickbaitArray[i];
+    itemTitle.setAttribute("href", linkArray[i]);
 
     var branding=document.createElement("div");
     branding.className="branding";
-    branding.innerHTML=advertiserArray[i];
+    branding.innerHTML=brandingArray[i];
+
 
     widgetItem.appendChild(thumbnail);
     widgetItem.appendChild(itemTitle);
     widgetItem.appendChild(branding);
-
 
     document.getElementById("widget").appendChild(widgetItem);
 
